b034e0ffbc3524bf8594db00fa81a9b570c68718
Matthew Roll <mproll@protonmail.com>
2020-07-25 16:06:55 -0700

    add staq

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { initStaq, withStaq } from 'staq'
+
+initStaq({
+  siteTitle: 'Staq Live Demo',
+  landingPageHeader: 'SaaS apps are great',
+  landingPageSubheader: 'You should totally subscribe',
+  firebaseConfig: {
+    apiKey: "AIzaSyA53q0l3Q68T6R19imxXl9SHOGxfHGhdOM",
+    authDomain: "staq-live-demo.firebaseapp.com",
+    databaseURL: "https://staq-live-demo.firebaseio.com",
+    projectId: "staq-live-demo",
+    storageBucket: "staq-live-demo.appspot.com",
+    messagingSenderId: "316093476373",
+    appId: "1:316093476373:web:44a44c5cc8c5a1f782681c",
+    measurementId: "G-05585GF15F"
+  }
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  withStaq(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ),
   document.getElementById('root')
 );
 
